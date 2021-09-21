f01765c1ca86e23e7f5d4bbb8255db9a436f221e
Carlos Mora <camoraf@unal.edu.co>
2021-09-21 13:36:48 -0500

    Variables in python

---
+++ Python/variables.py
@@ -0,0 +1,18 @@
+x = 100
+
+#Case Sensitive: Son variables totalmente diferentes
+book = "Digital Fortress"
+Book = "Digital Fortress"
+
+y, music = 100, "nirvana" #Almacenar 2 variables en una sola línea
+
+#Conventions
+bookName = "I robot" #Camel Case
+book_name = "Digital Stack" #Snake Case
+BookName = "sadas" #Pascal  Case
+
+PI = 3.1416 #Palabras en mayúscula se usa para constantes
+MY_NAME = "Carlos"
+
+book_name = "I robot"
+book_name = 1412 #Python es un lenguaje de tipado dinámico
