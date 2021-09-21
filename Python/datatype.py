e2f5969200296f371a88c6688b89a16d307a07b3
Carlos Mora <camoraf@unal.edu.co>
2021-09-21 13:20:52 -0500

    All datatypes in python

--- Python/datatype.py
+++ Python/datatype.py
@@ -13,7 +13,35 @@ print(30)
 print(30.5)
 
 # Boolean
-false
-true
+False
+True
 
-# Lists
+# Lists, las listas pueden cambiar
+[10, 20, 40, 50]
+['hola','bye','adios']
+[10,'hello',True, 10.2]
+[]
+
+# Tuples, las tuplas no pueden cambiar
+(10,20,30,55)
+()
+
+# Dictionaries
+{
+    "name": "Ryan",
+    "apellido": "Ray",
+    "apodo": "Fazt"
+}
+
+{
+    "lat": 121,
+    "long": 234
+}
+
+print(type({
+    "name": "Ryan",
+    "apellido": "Ray",
+    "apodo": "Fazt"
+}))
+
+None #Tipo de dato no definido
