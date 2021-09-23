1fe57068e609b36cf7270e99ce53fcb40caa164d
Carlos Mora <camoraf@unal.edu.co>
2021-09-23 12:30:12 -0500

    Variables en python

---
+++ Python/strings.py
@@ -0,0 +1,10 @@
+myStr = "Hello World"
+
+#print(dir(myStr))
+
+print(myStr.upper())
+print(myStr.lower())
+print(myStr.capitalize())
+print(myStr.replace('Hello', 'bye').upper())
+print(myStr.count('l')) 
+
