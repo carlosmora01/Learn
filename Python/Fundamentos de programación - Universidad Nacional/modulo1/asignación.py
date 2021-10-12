ba546fb43eb7158c188102a004b3ae75a21391ab
Carlos Mora <camoraf@unal.edu.co>
2021-10-12 11:57:19 -0500

    fundamentos de programación

---
+++ Python/Fundamentos de programación - Universidad Nacional/modulo1/asignación.py
@@ -0,0 +1,9 @@
+a, b = 3, 4
+c = a+b
+
+print(c)
+
+a+=2
+b-=4
+c*=a
+print(a, b, c)
---
+++ Python/Fundamentos de programación - Universidad Nacional/modulo1/operadores.py
@@ -0,0 +1,23 @@
+# Suma = + 
+# Resta = - 
+# Multiplicación = * 
+# exponenciación = **
+# División(Real) = /
+# División (Entera ) = //
+# Módulo = %  Ejemplo(7%2 es 1)
+
+#Precedencia
+  
+  # Muy alta ()
+  # alta **
+  # Media *, /, //, %
+  # baja +, -
+
+a = 5+4*2
+b = (5 + 4) * 2
+c = 6 - 13 % 5 + 2**3
+d = ((8+3) // 2) ** 2 - (7-4)*3+5
+e = 17//3+12**2-((5+8)*2)
+
+print(a, b, c, d, e )
+print(27//4*4 + (8**2-2 * 3)* 2 - 17)
